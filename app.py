85fc965055ed54cd96b5361ab43797374b8730ab
Evan Baird <mrcartoonster@gmail.com>
2020-09-16 11:51:24 -0600

    📦 NEW: part1 #time 30m #comment Working with sessions

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, escape, render_template
+from flask import Flask, escape, render_template, request, session
 
 app = Flask(__name__)
+
+app.secret_key = "BAD_SECRET_KEY"
 
 
 @app.route("/")
@@ -29,6 +31,21 @@ def display_blog_post(post_id):
     return f"<h1>Blog Post #{post_id}...<h1>"
 
 
-@app.route("/add_stock")
+@app.route("/add_stock", methods=["GET", "POST"])
 def add_stock():
+    if request.method == "POST":
+        # DEBUG - Print the form data to the console.
+        for key, value in request.form.items():
+            print(f"{key}: {value}")
+
+        # Save the form data to the session object.
+        session["stock_symbol"] = request.form["stock_symbol"]
+        session["number_of_shares"] = request.form["number_of_shares"]
+        session["share_price"] = request.form["share_price"]
+
     return render_template("add_stock.html")
+
+
+@app.route("/stocks")
+def list_stocks():
+    return render_template("stocks.html")
