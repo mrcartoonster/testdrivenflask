36400b5633237e2530bb905a0f518478e9103422
Evan Baird <mrcartoonster@gmail.com>
2020-09-16 12:50:36 -0600

    📦 NEW: part1 #time 30m #comment adding tests

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, escape, render_template, request, session
+from flask import Flask, redirect, render_template, request, session, url_for
 
 app = Flask(__name__)
 
@@ -11,37 +11,14 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-
-@app.route("/stocks/")
-def stocks():
-    return "<h2>Stock List...</h2>"
-
-
-@app.route("/hello/<message>")
-def hello_message(message):
-    return f"<h1>Welcome {escape(message)}!</h1>"
-
-
-@app.route("/blog_posts/<int:post_id>")
-def display_blog_post(post_id):
-    return f"<h1>Blog Post #{post_id}...<h1>"
-
-
 @app.route("/add_stock", methods=["GET", "POST"])
 def add_stock():
     if request.method == "POST":
-        # DEBUG - Print the form data to the console.
-        for key, value in request.form.items():
-            print(f"{key}: {value}")
-
         # Save the form data to the session object.
         session["stock_symbol"] = request.form["stock_symbol"]
         session["number_of_shares"] = request.form["number_of_shares"]
-        session["share_price"] = request.form["share_price"]
+        session["purchase_price"] = request.form["purchase_price"]
+        return redirect((url_for("list_stocks")))
 
     return render_template("add_stock.html")
 
