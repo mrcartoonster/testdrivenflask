a1fe7c1312d74adecde7616355493305d87682ec
Evan Baird <mrcartoonster@gmail.com>
2020-09-16 09:53:08 -0600

    First Commit

---
+++ flask-stock-portfolio-code/app.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, escape, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return "<h2>About this application...</h2>"
+
+
+@app.route("/stocks/")
+def stocks():
+    return "<h2>Stock List...</h2>"
+
+
+@app.route("/hello/<message>")
+def hello_message(message):
+    return f"<h1>Welcome {escape(message)}!</h1>"
+
+
+@app.route("/blog_posts/<int:post_id>")
+def display_blog_post(post_id):
+    return f"<h1>Blog Post #{post_id}...<h1>"
