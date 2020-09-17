c634d040abccdd96d9c5985191cebbed13365bf4
Evan Baird <mrcartoonster@gmail.com>
2020-09-17 11:32:01 -0600

    📦 NEW: TES-1 #time 30m #comment moving over to venv

--- tests/unit/test_app.py
+++ tests/unit/test_app.py
@@ -12,3 +12,17 @@ def test_index_page():
         assert response.status_code == 200
         assert b"Flask Stock Portfolio App" in response.data
         assert b"Welcome to the Flask Stock Portfolio App!" in response.data
+
+
+def test_about_page():
+    """GIVEN a Flask application WHEN the '/about' page is requested (GET) THEN
+    check the response is valid."""
+    with app.test_client() as client:
+        response = client.get("/about")
+        assert b"Flask Stock Portfolio App" in response.data
+        assert b"About" in response.data
+        assert (
+            b"This application is built using the Flask web framework."
+            in response.data
+        )
+        assert b"Course developed by TestDriven.io." in response.data
