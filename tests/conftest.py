1bc2798d9b88c1373a845f82ccd6089b93a9604b
Evan Baird <mrcartoonster@gmail.com>
2020-10-08 10:26:16 -0600

    📦 NEW: TES-4 #time 30m #comment Adding tests for new stocks

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,37 +8,13 @@ from project.models import Stock, User
 
 
 @pytest.fixture(scope="module")
-def test_client():
-    flask_app = create_app()
-    flask_app.config.from_object("config.TestingConfig")
-    flask_app.extensions["mail"].suppress = True
-
-    # Create a test client using the Flask application configured for testing.
-    with flask_app.test_client() as testing_client:
-        # Establish an application context before accessing the logger.
-        with flask_app.app_context():
-            flask_app.logger.info(
-                "Creating database tables in test_client fixture",
-            )
-
-            # Create the database and the database tables.
-            db.create_all()
-
-        yield testing_client  # Where the tests are going one.
-
-        with flask_app.app_context():
-            db.drop_all()
-
-
-@pytest.fixture(scope="module")
-def new_stock(test_client_with_app_context):
-    """Base Stocks to add."""
-    stock = Stock("AAPL", "16", "406.78", 17, datetime(2020, 7, 18))
+def new_stock():
+    stock = Stock("AAPL", "16", "406.78")
     return stock
 
 
 @pytest.fixture(scope="module")
-def new_user():
+def new_user(test_client_with_app_context):
     user = User("patrick@email.com", "FlaskIsAwesome123")
     return user
 
@@ -78,9 +54,9 @@ def confirm_email_default_user(test_client, log_in_default_user):
     db.session.add(user)
     db.session.commit()
 
-    yield user  # This is where the testing happens!
+    yield user  # this is where the testing happens!
 
-    # Mark the user as not having their email address confirmed(clean up)
+    # Mark the user as not having their email address confirmed (clean up)
     user = User.query.filter_by(email="patrick@gmail.com").first()
     user.email_confirmed = False
     user.email_confirmed_on = None
@@ -88,25 +64,49 @@ def confirm_email_default_user(test_client, log_in_default_user):
     db.session.commit()
 
 
-@pytest.fixture(scope="function")
-def afterwards_reset_default_user_password():
-    yield  # this is where the testing is happening!
-
-    # Since a test using this fixture could change the password for the default
-    # user, rerset the password back to the default password
-    user = User.query.filter_by(email="patrick@gmail.com").first()
-    user.set_password("FlaskIsAwesome123")
-    db.session.add(user)
-    db.session.commit()
-
-
 @pytest.fixture(scope="module")
-def test_client_with_app_context():
-    """Haven't used this yet."""
+def test_client():
     flask_app = create_app()
     flask_app.config.from_object("config.TestingConfig")
     flask_app.extensions["mail"].suppress = True
 
+    # Create a test client using the Flask application configured for testing
     with flask_app.test_client() as testing_client:
+        # Establish an application context before accessing the logger and db
         with flask_app.app_context():
-            yield testing_client
+            flask_app.logger.info(
+                "Creating db tables in test_client fixture...",
+            )
+
+            # Create the db and the db table(s)
+            db.create_all()
+
+        yield testing_client  # this is where the testing happens!
+
+        with flask_app.app_context():
+            db.drop_all()
+
+
+@pytest.fixture(scope="module")
+def test_client_with_app_context():
+    flask_app = create_app()
+    flask_app.config.from_object("config.TestingConfig")
+    flask_app.extensions["mail"].suppress = True
+
+    # Create a test client using the Flask application configured for testing
+    with flask_app.test_client() as testing_client:
+        # Establish an application context before accessing the logger and db
+        with flask_app.app_context():
+            yield testing_client  # this is where the testing happens!
+
+
+@pytest.fixture(scope="function")
+def afterwards_reset_default_user_password():
+    yield  # this is where the testing happens!
+
+    # Since a test using this fixture could change the password for the
+    # default_user, reset the password back to the default password
+    user = User.query.filter_by(email="patrick@gmail.com").first()
+    user.set_password("FlaskIsAwesome123")
+    db.session.add(user)
+    db.session.commit()
--- tests/functional/test_stocks.py
+++ tests/functional/test_stocks.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 
-def test_get_add_stock_page(test_client):
+def test_get_add_stock_page(test_client, log_in_default_user):
     """GIVEN a Flask application WHEN the '/add_stock' page is requested
     (GET) THEN check the response is valid."""
     response = test_client.get("/add_stock")
@@ -11,9 +11,10 @@ def test_get_add_stock_page(test_client):
     assert b"Stock Symbol (required):" in response.data
     assert b"Number of Shares (required):" in response.data
     assert b"Purchase Price (required) ($):" in response.data
+    assert b"Purchase Date" in response.data
 
 
-def test_post_add_stock_page(test_client):
+def test_post_add_stock_page(test_client, log_in_default_user):
     """GIVEN a Flask application WHEN the '/add_stock' page is posted
     (POST) THEN check that the user is redirected to the '/list_stocks'
     page."""
@@ -35,3 +36,34 @@ def test_post_add_stock_page(test_client):
     assert b"AAPL" in response.data
     assert b"23" in response.data
     assert b"432.17" in response.data
+    assert b"Added new stock (AAPL)!" in response.data
+
+
+def test_get_add_stock_page_not_logged_in(test_client):
+    """GIVEN a Flask application WHEN the '/add_stock' page is requested
+    (GET) when the user is not logged in THEN check that the user is
+    redirected to the login page."""
+    response = test_client.get("/add_stock", follow_redirects=True)
+    assert response.status_code == 200
+    assert b"Add a Stock" not in response.data
+
+
+def test_post_add_stock_page_not_logged_in(test_client):
+    """GIVEN a Flask application WHEN the '/add_stock' page is posted to
+    (POST) when the user is not logged in THEN check that the user is
+    redirected to the login page."""
+    response = test_client.post(
+        "/add_stock",
+        data={
+            "stock_symbol": "AAPL",
+            "number_of_shares": "23",
+            "purchase_price": "432.17",
+            "purchase_date": "07/24/2020",
+        },
+        follow_redirects=True,
+    )
+    assert response.status_code == 200
+    assert b"List of Stocks:" not in response.data
+    assert b"Added new stock (AAPL)!" not in response.data
+    assert b"Please log in to access this page." in response.data
+    assert b"Please log in to access this page." in response.data
